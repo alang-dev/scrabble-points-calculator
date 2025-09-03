a1b25713c1a462bd23323a7767f24c57b0157db4
Kien Alang <kien.alang@zuhlke.com>
2025-09-03 11:25:02 +0700

    Add ci profile to resolve db connection issue

--- backend/src/test/java/com/scrabble/ScrabbleApplicationTest.java
+++ backend/src/test/java/com/scrabble/ScrabbleApplicationTest.java
@@ -2,8 +2,10 @@ package com.scrabble;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("${spring.profiles.active:ci}")
 class ScrabbleApplicationTest {
 
   @Test
